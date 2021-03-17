cbb713d4f622e600955c30873ad94698f76452f3
BajicMladen <bajicmladen@uns.ac.rs>
2021-03-17 21:50:39 +0100

    List with two users created; methods to get single user and to get all users created

---
+++ Dotnet-CRUD/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Dotnet_CRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dotnet_CRUD.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private static List<User> users = new List<User>
+        {
+            new User{},
+            new User {Id=1,Name="Nikola",LastName="Nikolic", age=33}
+        };
+
+        [HttpGet("GetAll")]
+        public IActionResult Get()
+        {
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id)
+        {
+            return Ok(users.FirstOrDefault(user => user.Id==id));
+        }
+
+    }
+}
--- Dotnet-CRUD/Models/User.cs
+++ Dotnet-CRUD/Models/User.cs
@@ -7,5 +7,15 @@ namespace Dotnet_CRUD.Models
 {
     public class User
     {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "Mladen";
+
+        public string LastName { get; set; } = "Bajic";
+
+        public int age { get; set; } = 21;
+
+       
     }
 }
